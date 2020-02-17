ea4a3aaa9b2406fa84a608790768547effea8c08
Abraham Rager Gomez Flores <ag261y@att.com>
2020-02-17 01:59:29 -0600

    usuario y token listo

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
+
+//body parser
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// Importar Rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // conexion a la BD
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -11,14 +23,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
     console.log('Base de Datos: \x1b[32m%s\x1b[0m', 'online');
 });
 
-
-//Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: 'true',
-        mensaje: 'peticion realzada correctamente'
-    });
-});
+// Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 //Escuchar peticion
 app.listen(3000, () => {
---
+++ middlewares/autenticacion.js
@@ -0,0 +1,26 @@
+var jwt = require('jsonwebtoken');
+var SEED = require('../config/config').SEED;
+
+// =========================================
+// Verficar token
+// =========================================
+
+exports.verificaToken = function(req, res, next) {
+    var token = req.query.token;
+
+    jwt.verify(token, SEED, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({
+                ok: false,
+                mensaje: 'token invalido',
+                errors: err
+            });
+        }
+        req.usuario = decoded.usuario;
+        next();
+        // res.status(200).json({
+        //     ok: true,
+        //     decoded: decoded
+        // });
+    });
+};
